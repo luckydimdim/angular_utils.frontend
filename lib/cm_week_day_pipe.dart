4b0d59d98bd6e5d22ae38e3edcfa182b1abe4b50
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-04-27 13:54:02 +0300

    week pipe

---
+++ lib/cm_week_day_pipe.dart
@@ -0,0 +1,1 @@
+export 'src/cm_week_day_pipe.dart';
--- lib/pipes.dart
+++ lib/pipes.dart
@@ -1,2 +1,4 @@
 export 'cm_format_money_pipe.dart' show CmFormatMoneyPipe;
 export 'cm_format_currency_pipe.dart' show CmFormatCurrencyPipe;
+export 'cm_spent_time.dart' show CmSpentTimePipe;
+export 'cm_week_day_pipe.dart' show CmWeekDayPipe;
