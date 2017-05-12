f036665f4df3a33df55b842e5ef4a3daeed532f0
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-12 10:56:34 +0300

    money pipe fix

--- lib/src/cm_format_money_pipe.dart
+++ lib/src/cm_format_money_pipe.dart
@@ -6,7 +6,10 @@ import 'package:intl/intl.dart';
  * Пайпа для преобразования числовых значений к денежному формату
  */
 class CmFormatMoneyPipe extends PipeTransform {
-  final formatter = new NumberFormat('###,###.00', 'ru_RU');
+  final formatter = new NumberFormat('###,##0.00', 'ru_RU');
 
-  String transform(num value) => '${formatter.format(value)}';
+  String transform(num value){
+    return '${formatter.format(value)}';
+  }
+
 }
